91972cb12ab49d47c48c6681951b281af6e9b51a
Paul Richardson <developer@photographerafoot.com>
2021-05-28 08:45:47 +1000

    CHG: Reformat

--- LocalNotifications/ViewController.swift
+++ LocalNotifications/ViewController.swift
@@ -46,7 +46,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 		var dateComponents = DateComponents()
 		dateComponents.hour = 10
 		dateComponents.minute = 30
-//		let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+		//		let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
 		let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
 
 		let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
